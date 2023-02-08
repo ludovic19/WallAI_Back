be303ca7337f5d3b65dbf616002efb95fbb00828
HichamHajla <hichem500@hotmail.com>
2023-02-09 00:40:29 +0100

    Version 1 finalised

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,6 +1,18 @@
 import express from "express";
 import verify from "../Middlewares/verify.js";
 import Users from "../MongoDB/models/users.js";
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+      cb(null, 'public/images/');
+    },
+    filename: function(req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now() +  '.' + file.originalname.split('.').pop());
+    }
+  });
+  
+  const upload = multer({ storage: storage });
 
 const userRoutes = express.Router()
 
@@ -31,8 +43,15 @@ userRoutes.get('/users/:id', (req, res) => {
     .catch(err => res.json(err))
 })
 
-userRoutes.put('/users/:id', async (req,res) => {
-    await Users.findOneAndUpdate({_id : req.params.id}, {$set : req.body}, {new : true})
+userRoutes.put('/users/:id', upload.single('image'), async (req,res) => {
+    await Users.findOneAndUpdate({_id : req.params.id}, 
+        {$set :{username: req.body.username,
+         image: "/public/images/" + req.file.filename,
+         first_name : req.body.first_name,
+         last_name : req.body.last_name,
+         email : req.body.email
+            }}, 
+         {new : true})
     .then(newUser => res.json(newUser))
     .catch(err => res.json(err))
 })
